ce1c292755f2600d41c3bce0a5302cc0bd36e96a
ryd <724533614@qq.com>
2023-03-16 15:22:19 +0800

    3.16

---
+++ demo.py
@@ -0,0 +1,7 @@
+# n,k = map(int, input().split())
+# string = list(input().split(' '))
+string = input()
+print(list(string))
+breakpoint()
+nums = list(map(int, input().split()))
+print(n,k,nums)
--- 动态规划/518零钱兑换II.py
+++ 动态规划/518零钱兑换II.py
@@ -1,3 +1,5 @@
+"""给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+"""
 from typing import List
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
@@ -5,7 +7,7 @@ class Solution:
         dp = [0] * (amount + 1)
         dp[0] = 1
         for v in coins:
-            for j in range(v, amount+1):
+            for j in range(v, amount+1): # 正序遍历
                 dp[j] += dp[j-v]
         print(dp)
         return dp[-1]
